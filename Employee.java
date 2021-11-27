f7e91695a045851971081a9df5133989d1139284
AmarSinghBaghel <amar.hope170@gmail.com>
2021-11-27 23:36:22 +0530

    add new java file

---
+++ Employee.java
@@ -0,0 +1,33 @@
+package com.greatlearning.corejava;
+
+public class Employee {
+	
+		  Employee(){
+
+		    this(10);
+
+		    System.out.print(" hello");
+
+		  }
+
+		  Employee(int x){
+
+		    System.out.print("hi");
+
+		  }
+
+		}
+
+		 
+
+class amar {
+
+		  public static void main(String args[]) {
+
+			  Employee em = new Employee();
+
+		    }
+
+		}
+
+
